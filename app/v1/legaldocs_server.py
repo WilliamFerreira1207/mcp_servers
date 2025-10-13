70b65c44842d126936cde7858abcd733a47c26b3
William Ferreira <wferreira@gbm.net>
2025-10-13 16:20:26 -0400

    Tool de Upload File

--- app/v1/legaldocs_server.py
+++ app/v1/legaldocs_server.py
@@ -1,8 +1,10 @@
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv
+from schemas.response_schemas import LD_GetTemplatesResponse, LD_UploadTemplateResponse
+import base64
+from io import BytesIO
 import os
 import requests
-from schemas.response_schemas import LD_GetTemplatesResponse
 
 load_dotenv()  # Carga las variables de entorno desde el archivo .env
 
@@ -23,6 +25,57 @@ def get_available_temples() -> str:
     except Exception as e:
         return "Error retrieving legal documents"
 
+@mcp.tool(
+    name="upload_legal_doc_template",
+    structured_output=True
+)
+def upload_legal_doc_template(base64_content: str, filename: str) -> LD_UploadTemplateResponse:
+    """
+    Upload a legal document template to the external service.
+    Args:
+        base64_content (str): The base64 encoded content of the template.
+        filename (str): The name of the file to be uploaded.
+
+    Returns:
+    """
+    try:
+        print(f"Uploading template: {filename}")
+        file_bytes = base64.b64decode(base64_content)
+        print(f"Decoded file size: {len(file_bytes)} bytes")
+        files = {
+            'file': (filename if filename.endswith(".pdf") else f"{filename}.pdf", BytesIO(file_bytes), 'application/pdf')
+        }
+        print(f"Prepared files for upload: {files['file'][0]}, size: {len(file_bytes)} bytes")
+        
+        data = {
+            'name': filename
+        }
+        print(f"Uploading template: {filename}")
+        response = requests.post(f"{LEGAL_DOCS_URL}/upload-template", files=files, data=data)
+        print(f"Response status code: {response.status_code}")
+        print(f"Response content: {response.json()}")
+        if response.status_code == 200:
+            return LD_UploadTemplateResponse(
+                result=response.json().get("message", "Template uploaded successfully"),
+                filename=filename,
+                status_code=response.status_code,
+                success=True,
+                
+            )
+        else:
+            return LD_UploadTemplateResponse(
+                result=f"Failed to upload template: {response.text}",
+                filename=filename,
+                status_code=response.status_code,
+                success=False
+            )
+    except Exception as e:
+        return LD_UploadTemplateResponse(
+            result="Error uploading template",
+            filename=filename,
+            status_code=500,
+            success=False
+        )
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
--- schemas/response_schemas.py
+++ schemas/response_schemas.py
@@ -3,3 +3,9 @@ from typing import List, Optional
 
 class LD_GetTemplatesResponse(BaseModel):
     templates: List[str]
+    
+class LD_UploadTemplateResponse(BaseModel):
+    filename: str
+    status_code: int
+    success: bool
+    result: str
